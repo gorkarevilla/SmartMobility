b9a517db4c0c5017799b94a763eb871238df2b39
Gorka Revilla <gorkarevilla@gmail.com>
2017-04-10 14:48:59 +0200

    Add the clean file method

--- behaviour/views.py
+++ behaviour/views.py
@@ -9,6 +9,10 @@ from django.views.decorators.http import require_http_methods
 
 from .forms import UploadFileForm
 
+import csv
+from datetime import datetime
+from io import BytesIO
+
 # Create your views here.
 @require_http_methods(["GET"])
 def index(request):
@@ -22,7 +26,10 @@ def upload(request):
 	if request.method == 'POST':
 		formupload = UploadFileForm(request.POST, request.FILES)
 		if formupload.is_valid():
-			# read_file(request.FILES['file'])
+			spacer = " "
+			gaptime = 60
+			positions = clean_file(request.FILES['file'],spacer)
+			determine_trips(positions,gaptime)
 			messages.success(request,"File Uploaded Correctly")
 			return HttpResponseRedirect('maposm.html')
 		else:
@@ -34,5 +41,47 @@ def upload(request):
 
 
 #Process the file
-#def read_file(file):
+#Format: 
+# dateTime device_id id latitude longitude speed
+# 2017-01-24T09:49:24.063Z za0 c3e0b6fcd96d0a329903887ec39cb5835780db17 40.42951587 -3.64513278 0
+# 
+def clean_file(file,spacer):
 
+	positions = []
+
+	reader = csv.reader(file, delimiter=str(spacer))
+
+	cleanfile = BytesIO()
+	writter = csv.writer(cleanfile, delimiter=str(spacer))
+	counter = 0
+	#for line in file:
+	for line in reader:
+		#Check the line
+		try:
+			point = []
+			# 2017-02-02T19:18:36.063Z
+			timestamp = datetime.strptime(line[0], '%Y-%m-%dT%H:%M:%S.%fZ')
+			point.append(timestamp.strftime("%Y-%m-%d %H:%M:%S"))
+			point.append(line[1])
+			point.append(line[3])
+			point.append(line[4])
+
+			positions.append(point)
+
+			#writter.writerow([strtimestamp, device_id, latitude, longitude])
+			
+		except ValueError:
+			# 2017-01-24
+			#datetime1 = datetime.strptime(line[0], '%Y-%m-%d')
+			continue
+
+		counter+=1
+	print counter
+
+	return positions
+
+# Determine Trips
+def determine_trips(positions,gaptime):
+	
+	for pos in positions:
+		print pos
