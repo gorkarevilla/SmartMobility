d1ddf98db1f9148278d847428ac85efbfff8543a
Gorka Revilla <gorkarevilla@gmail.com>
2017-05-13 19:02:51 +0200

    Calculate trips from ddbb

--- behaviour/views.py
+++ behaviour/views.py
@@ -27,16 +27,23 @@ from geopy.distance import vincenty
 # Create your views here.
 
 
+#####
+#####  WEBPAGE SERVING FUNCTIONS
+#####
+
+#INDEX.HTML
 @require_http_methods(["GET"])
 def index(request):
 	userform = LoginForm()
 	return render (request, 'behaviour/index.html', {'userform': userform})
 
+#MAPOSM.HTML
 @require_http_methods(["GET"])
 def maposm(request):
 	userform = LoginForm()
 	return render (request, 'behaviour/maposm.html', {'userform': userform})
 
+#UPLOAD.HTML
 def upload(request):
 	if request.method == 'POST':
 		formupload = UploadFileForm(request.POST, request.FILES)
@@ -59,29 +66,7 @@ def upload(request):
 		return render(request, 'behaviour/upload.html', {'formupload': formupload, 'userform': userform})
 
 
-def load_trips(request):
-	print("Getting positions...")
-	gaptime = 120
-	pointsqs = get_points()
-
-	# IF is not empty
-	if(pointsqs.exists()):
-
-		ntrips = create_trips(request,pointsqs,gaptime)
-
-		if(ntrips == 0):
-			messages.error(request, 'No trips to be processed!')
-			return HttpResponseRedirect('maposm.html')
-		else:
-			print("Number of trips loaded: "+str(ntrips))
-			messages.success(request,"Trips Generated Correctly")
-			return HttpResponseRedirect('maposm.html')
-
-	else:
-		messages.error(request, 'No points to be processed!')
-		returnHttpResponseRedirect('maposm.html')
-
-
+#DISPLAY.HTML
 @require_http_methods(["GET"])
 def display(request):
 	userform = LoginForm()
@@ -99,6 +84,7 @@ def user_logout(request):
 @require_http_methods(["GET"])
 def clean_DDBB(request):
 	Trips.objects.all().delete()
+	set_all_points_noused()
 	print("All Data Deleted!")
 	return HttpResponseRedirect('maposm.html')
 
@@ -122,13 +108,22 @@ def downloadfile(request):
 	return response
 
 
-#Process the file
-#Format input: 
-# dateTime device_id id latitude longitude speed
-# 2017-01-24T09:49:24.063Z za0 c3e0b6fcd96d0a329903887ec39cb5835780db17 40.42951587 -3.64513278 0
+
+
+
+#####
+#####  BUTTON FUNCTIONS
+#####
+
+
+# When the file is uploaded to the server
+# Process the file
+# Format input: 
+#  	dateTime device_id id latitude longitude speed
+#  	2017-01-24T09:49:24.063Z za0 c3e0b6fcd96d0a329903887ec39cb5835780db17 40.42951587 -3.64513278 0
 #
-#Format in the model:
-# Points = 	[
+# Format in the model:
+# 	Points = 	[
 #					[timestamp] 			[device_id]	[latitude]		[longitude] 	[speed] [hasTrip]
 #					[2017-01-24 09:49:24] 	[za0] 		[40.4251587] 	[-3.64513278] 	[0]		[False]
 # 				]
@@ -209,16 +204,28 @@ def save_points(file):
 	return counter
 
 
-def neighborhood(iterable):
-	iterator = iter(iterable)
-	prev_item = None
-	current_item = next(iterator)  # throws StopIteration if empty.
-	for next_item in iterator:
-		yield (prev_item, current_item, next_item)
-		prev_item = current_item
-		current_item = next_item
-	yield (prev_item, current_item, None)
+# Load trips from the DDBB
+def load_trips(request):
+	print("Getting positions...")
+	gaptime = 120
+	pointsqs = get_points()
 
+	# IF is not empty
+	if(pointsqs.exists()):
+
+		ntrips = create_trips(request,pointsqs,gaptime)
+
+		if(ntrips == 0):
+			messages.error(request, 'No trips to be processed!')
+			return HttpResponseRedirect('maposm.html')
+		else:
+			print("Number of trips loaded: "+str(ntrips))
+			messages.success(request,"Trips Generated Correctly")
+			return HttpResponseRedirect('maposm.html')
+
+	else:
+		messages.error(request, 'No points to be processed!')
+		returnHttpResponseRedirect('maposm.html')
 
 
 # from a queryset with points create a list of points for each trip
@@ -231,6 +238,7 @@ def create_trips(request,pointsqs,gaptime):
 	print("Creating trips...")
 
 	ntrips = 0
+	insertedtrips = 0
 
 	trip = []
 	
@@ -239,6 +247,7 @@ def create_trips(request,pointsqs,gaptime):
 
 	iterator = pointsqs.values_list('id','timestamp','device_id','latitude','longitude','speed').iterator()
 
+	# Everything in the same transaction
 	with transaction.atomic():
 
 		for prevpoint,thispoint,nextpoint in neighborhood(iterator):
@@ -257,6 +266,7 @@ def create_trips(request,pointsqs,gaptime):
 				# If the time is close, is part of a trip
 				if (timedifference(nextdate,thisdate)<timedelta(seconds=gaptime) and thisdevice == nextdevice):
 
+					print(thisdevice +" = "+nextdevice)
 					#List of points for trip [id][timestamp][device_id][latitud][longitude]
 					point = []
 					point.append(thispoint[0]) # id (of the point)
@@ -289,7 +299,9 @@ def create_trips(request,pointsqs,gaptime):
 						set_point_used(thispoint[0])					
 						ntrips+=1
 						isLastPoint=0
-						save_trip(request,trip)
+						# Save or print an error
+						if(save_trip(request,trip) == 0):
+							print("ERROR: One Trip can not be saved")
 						trip=[]
 					
 
@@ -304,6 +316,76 @@ def create_trips(request,pointsqs,gaptime):
 
 
 
+#####
+#####  UTILITY CLASSES
+#####
+class geoposition():
+
+	city = None
+	citytype = None
+	country = None
+
+	def __init__(self, latitude, longitude):
+
+		locjson = None
+		try:
+			sleep(1) # The API need at least 1 sec between each query
+			geolocator = Nominatim()
+			location = geolocator.reverse(str(latitude) + ", "+ str(longitude))
+			locjson = json.loads(json.dumps(location.raw))
+			self.city = locjson['address']['city']
+			self.citytype="city"
+		except GeocoderTimedOut:
+			print("Error: Geocode time out")
+		except KeyError:
+			try:
+				self.city = locjson['address']['town']
+				self.citytype="town"
+			except KeyError:
+				try:
+					self.city = locjson['address']['village']
+					self.citytype="village"
+				except KeyError:
+					try:
+						self.city = locjson['address']['neighbourhood']
+						self.citytype="neighbourhood"
+					except KeyError:
+						try:
+							self.city = locjson['address']['hamlet']
+							self.citytype="hamlet"
+						except KeyError:
+							#print(locjson)
+							self.city = "Unknown"
+							self.citytype="Unknown"
+		except Exception as e:
+			print("ERROR: " + str(e))
+		try:
+			self.country = locjson['address']['country']
+		except KeyError:
+			self.country = "Unknown"
+
+
+
+#####
+#####  UTILITY FUNCTIONS
+#####
+
+# Get the previous, the current and the next item with the iterable
+# Usage:
+# 		for prev,item,next in neighborhood(l):
+#       	print prev, item, next
+#
+def neighborhood(iterable):
+	iterator = iter(iterable)
+	prev_item = None
+	current_item = next(iterator)  # throws StopIteration if empty.
+	for next_item in iterator:
+		yield (prev_item, current_item, next_item)
+		prev_item = current_item
+		current_item = next_item
+	yield (prev_item, current_item, None)
+
+
 # Returns the deltatime between two datatimes
 def timedifference(t1,t2):
 	if(t1<t2):
@@ -311,6 +393,10 @@ def timedifference(t1,t2):
 	else:
 		return t1-t2
 
+
+#####
+#####  DDBB FUNCTIONS
+#####
 
 # [timestamp, device_id, latitude, longitude, speed]
 def insert_points(positions):
@@ -326,10 +412,99 @@ def delete_points(request):
 
 
 
-
+# Save the trip in the model
+# Trip is a list of points: [id][timestamp][device_id][latitude][longitude][speed]
 def save_trip(request,trip):
-	#print("Saving trip...")
-	return
+	print("Saving trip...")
+
+	if(len(trip) == 0):
+		print("ERROR: List Empty!")
+		return 0
+	
+	# Get the first and last points
+	firstpoint = trip[0]
+	lastpoint = trip[-1]
+
+	username = request.user
+	device_id = firstpoint[2]
+	firsttimestamp = firstpoint[1]
+	lasttimestamp = lastpoint[1]
+	firstpointlatitude = firstpoint[3]
+	firstpointlongitude = firstpoint[4]
+	lastpointlatitude = lastpoint[3]
+	lastpointlongitude = lastpoint[4]
+
+	#Create the Geom (list of points)
+	listpoints = []
+	for p in trip:
+		point = Point(float(p[4]),float(p[3])) # (longitude,latitude)
+		listpoints.append(point)
+
+
+
+	duration = timedifference(firsttimestamp,lasttimestamp).total_seconds()
+	distance = vincenty( (firstpointlatitude,firstpointlongitude), (lastpointlatitude,lastpointlongitude) ).meters
+	npoints = len(listpoints)
+
+	# no inserts
+	if(npoints < 2):
+		print("ERROR: Points less than 2!")
+		return 0
+	if(distance == 0 or duration == 0):
+		print("ERROR: Distance or duration is 0")
+		return 0
+
+	try:
+		velocity = (3.6)*(distance/duration)
+	except ZeroDivisionError:
+		print("ERROR: Zero Division, duration is zero")
+		velocity = 0
+
+
+	#Determine accelerations
+	gapac = 1.5
+	gapbk = 0.5
+	naccelerations = 0
+	nbreaks = 0
+	prevspeed=float(trip[0][5])
+	for completepoint in trip:
+
+		curspeed = float(completepoint[5])
+
+		# Lecture error? or first/last point?
+		if(curspeed !=0 and prevspeed !=0):
+			if(prevspeed*gapac < curspeed):
+				naccelerations+=1
+
+			if(prevspeed*gapbk > curspeed):
+				nbreaks+= 1
+
+		prevspeed=curspeed
+
+
+	firstposition = geoposition(firstpoint[3],firstpoint[4])
+	city = firstposition.city
+	country = firstposition.country
+	citytype = firstposition.citytype
+
+	print("Adding: "+ device_id + " Points: " + str(npoints) + " A("+str(naccelerations) +") "+ " B("+str(nbreaks) +") "+" city: " + city + "("+country+")")
+	
+	insert = Trips( username=username, device_id=device_id,
+		firsttimestamp=firsttimestamp, lasttimestamp=lasttimestamp,
+		firstpointlatitude=firstpointlatitude, firstpointlongitude=firstpointlongitude,
+		lastpointlatitude=lastpointlatitude, lastpointlongitude=firstpointlongitude,
+		geom=LineString(listpoints),
+		city=city, country=country,citytype=citytype,
+		duration=duration, distance=distance, velocity=velocity, npoints=npoints,
+		naccelerations=naccelerations,nbreaks=nbreaks
+	)
+
+	insert.save()
+
+	return 1
+
+
+
 
 
 # Save the trips in the model
@@ -575,3 +750,7 @@ def get_points():
 def set_point_used(id):
 
 	Points.objects.filter(id=id).update(hasTrip=True)
+
+# Set all the points available for creating trips
+def set_all_points_noused():
+	Points.objects.all().update(hasTrip=False)
